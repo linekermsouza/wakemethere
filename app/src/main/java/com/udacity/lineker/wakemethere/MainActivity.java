083121b0fefedf2b73b05689140b43d91b20ec6c
Lineker Souza <linekermsouza@gmail.com>
2019-01-12 02:54:00 -0400

    handle add place button calling PlacePicker; Configured Google Play Services API Key; Adjustments on gradle properties;

--- app/src/main/java/com/udacity/lineker/wakemethere/MainActivity.java
+++ app/src/main/java/com/udacity/lineker/wakemethere/MainActivity.java
@@ -1,13 +1,57 @@
 package com.udacity.lineker.wakemethere;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.udacity.lineker.wakemethere.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.addPlace.setOnClickListener(addPlaceListener);
     }
+
+    View.OnClickListener addPlaceListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_FINE_LOCATION);
+                return;
+            }
+            try {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Intent i = builder.build(MainActivity.this);
+                startActivityForResult(i, PLACE_PICKER_REQUEST);
+            } catch (GooglePlayServicesRepairableException e) {
+                Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+            } catch (GooglePlayServicesNotAvailableException e) {
+                Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+            } catch (Exception e) {
+                Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+            }
+        }
+    };
 }
