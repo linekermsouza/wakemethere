86541c3e3b489d8e88de28f21532e7b702adfa74
Lineker Souza <linekermsouza@gmail.com>
2019-01-16 02:01:35 -0400

    Added AppBarLayout and Toolbar components;

--- app/src/main/java/com/udacity/lineker/wakemethere/MainActivity.java
+++ app/src/main/java/com/udacity/lineker/wakemethere/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -64,6 +65,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
 
         binding.addPlace.setOnClickListener(addPlaceListener);
         mDb = AppDatabase.getInstance(getApplicationContext());
